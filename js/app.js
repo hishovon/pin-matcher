003db78ef32b00acfcf92148b6f701e1c62ddfcc
hishovon <akshovon112@gmail.com>
2023-02-15 20:40:50 +0600

    second-commit

--- js/app.js
+++ js/app.js
@@ -27,3 +27,65 @@ const displayPinField =document.getElementById('display-pin');
 displayPinField.value=pin;
 
 })
+
+
+
+document.getElementById('calculator').addEventListener('click', function(event){
+
+const number = event.target.innerText;   
+
+const typeNumberField = document.getElementById('typed-numbers');
+const previousTypeNumber = typeNumberField.value;
+
+if(isNaN(number))
+{
+    if(number == 'C'){
+        typeNumberField.value = '';
+    }
+    else if (number== '<' )
+    {
+        const digit=previousTypeNumber.split('');
+        digit.pop('');
+        const remainingNumbers=digit.join('');
+        typeNumberField.value=remainingNumbers;
+    }
+}
+else{
+  
+    const newTypedNumber = previousTypeNumber + number ;
+    typeNumberField.value=newTypedNumber;
+}
+
+
+})
+
+
+
+document.getElementById('verify-pin').addEventListener('click', function(){
+
+    const displayPinField = document.getElementById('display-pin');
+    const currentPin = displayPinField.value;
+
+
+   const typeNumberField= document.getElementById('typed-numbers');
+   const typedNumber=typeNumberField.value;
+
+
+   const successPin =document.getElementById('pin-success');
+   const failPin =document.getElementById('pin-fail');
+
+   if(typedNumber==currentPin){
+
+   
+    successPin.style.display='block';
+    failPin.style.display='none'
+   }
+
+else{
+   
+    failPin.style.display='block';
+    successPin.style.display='none';
+}
+
+
+})
